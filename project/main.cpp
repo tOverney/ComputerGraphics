13ed477b94a9385b99a1597947c329935f9a2784
tOverney <tristan.overney@epfl.ch>
2015-04-23 14:57:15 +0200

    changed stub

--- project/main.cpp
+++ project/main.cpp
@@ -1,50 +1,227 @@
 #include "icg_common.h"
-#include "FrameBuffer.h"
-#include "_floor/Floor.h"
-#include "_cube/Cube.h"
+#include "cube.h"
+#include "trackball.h"
+#include "_grid/grid.h"
 
-int window_width = 1280;
-int window_height = 720;
-FrameBuffer fb(window_width, window_height);
+typedef Eigen::Transform<float,3,Eigen::Affine> Transform;
+
+using namespace std;
+
 Cube cube;
-Floor shinyfloor;
+Grid grid;
 
-void init() {
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_MULTISAMPLE);
-    cube.init();    
-    // TODO: initialize framebuffer
-    // TODO: initialize shinyfloor with the FB texture
-    shinyfloor.init(/*???*/);
+int WIDTH = 800;
+int HEIGHT = 600;
+
+mat4 projection_matrix;
+mat4 view_matrix;
+mat4 trackball_matrix;
+
+Trackball trackball;
+
+mat4 OrthographicProjection(float left, float right, float bottom, float top, float near, float far){
+    assert(right > left);
+    assert(far > near);
+    assert(top > bottom);
+    mat4 ortho = mat4::Zero();
+    ortho(0, 0) = 2.0f / (right - left);
+    ortho(1, 1) = 2.0f / (top - bottom);
+    ortho(2, 2) = -2.0f / (far - near);
+    ortho(3, 3) = 1.0f;
+    ortho(0, 3) = -(right + left) / (right - left);
+    ortho(1, 3) = -(top + bottom) / (top - bottom);
+    ortho(2, 3) = -(far + near) / (far - near);
+
+    return ortho;
 }
 
-void display() {
-    glViewport(0,0,window_width,window_height);
+mat4 PerspectiveProjection(float fovy, float aspect, float near, float far){
+
+    // TODO 1: Create a perspective projection matrix given the field of view,
+    // aspect ratio, and near and far plane distances.
+    
+ 
+    assert(far > near);
+    
+    float f= 1 / tan((fovy/2)*M_PI/180.0);
+    float a11 = f/(aspect);
+    float a22 = f;
+    float a33 = (near + far)/(near-far);
+    float a34 = 2*far*near/(near-far);
+    float a43 = -1.0f;
+    
+   mat4 projection;
+    projection << a11, 0, 0, 0,
+                  0, a22, 0, 0,
+                  0, 0, a33, a34,
+                  0, 0, a43, 0;
+    
+    //mat4 projection = mat4::Identity();
+    return projection;
+}
+
+mat4 LookAt(vec3 eye, vec3 center, vec3 up) {
+    // We need a function that converts from world coordinates into camera coordiantes.
+    //
+    // Cam coords to world coords is given by:
+    // X_world = R * X_cam + eye
+    //
+    // inverting it leads to:
+    //
+    // X_cam = R^T * X_world - R^T * eye
+    //
+    // Or as a homogeneous matrix:
+    // [ r_00 r_10 r_20 -r_0*eye
+    //   r_01 r_11 r_21 -r_1*eye
+    //   r_02 r_12 r_22 -r_2*eye
+    //      0    0    0        1 ]
+
+    vec3 z_cam = (eye - center).normalized();
+    vec3 x_cam = up.cross(z_cam).normalized();
+    vec3 y_cam = z_cam.cross(x_cam);
+
+    mat3 R;
+    R.col(0) = x_cam;
+    R.col(1) = y_cam;
+    R.col(2) = z_cam;
+
+    mat4 look_at = mat4::Zero();
+    look_at.block(0, 0, 3, 3) = R.transpose();
+    look_at(3, 3) = 1.0f;
+    look_at.block(0, 3, 3, 1) = -R.transpose() * (eye);
+    return look_at;
+}
+
+// Gets called when the windows is resized.
+void resize_callback(int width, int height) {
+    WIDTH = width;
+    HEIGHT = height;
+
+    std::cout << "Window has been resized to " << WIDTH << "x" << HEIGHT << "." << std::endl;
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    // TODO 1: Use a perspective projection instead;
+
+    projection_matrix = PerspectiveProjection(45.0f, (GLfloat)WIDTH / HEIGHT, 0.1f, 100.0f);
+ /*
+  GLfloat top = 1.0f;
+  GLfloat right = (GLfloat)WIDTH / HEIGHT * top;
+  projection_matrix = OrthographicProjection(-right, right, -top, top, -10.0, 10.0f);
+*/
+}
+
+void init(){
+    // Sets background color.
+    glClearColor(/*gray*/ .937,.937,.937, /*solid*/1.0);
+    
+    cube.init();
+    grid.init();
+
+    // Enable depth test.
+    glEnable(GL_DEPTH_TEST);
+
+    // TODO 3: Once you use the trackball, you should use a view matrix that
+    // looks straight down the -z axis. Otherwise the trackball's rotation gets
+    // applied in a rotated coordinate frame.
+    // Uncomment lower line to achieve this.
+    view_matrix = LookAt(vec3(2.0f, 2.0f, 4.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+    //view_matrix = Eigen::Affine3f(Eigen::Translation3f(0.0f, 0.0f, -4.0f)).matrix();
+
+    trackball_matrix = mat4::Identity();
+    check_error_gl();
+}
+
+// Gets called for every frame.
+void display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    float ratio = window_width / (float) window_height;
-    static mat4 projection = Eigen::perspective(45.0f, ratio, 0.1f, 10.0f);
-    vec3 cam_pos(2.0f, 2.0f, 2.0f);
-    vec3 cam_look(0.0f, 0.0f, 0.0f);
-    vec3 cam_up(0.0f, 0.0f, 1.0f);
-    mat4 view = Eigen::lookAt(cam_pos, cam_look, cam_up);
-    mat4 VP = projection * view;
 
-    // TODO: mirror the camera position
-    // TODO: create new VP for mirrored camera
-    // TODO: render the cube using the mirrored camera
-    // HINT: this render will be done in the framebuffer texture (remember bind/unbind)
-    
-    shinyfloor.draw(VP);
-    cube.draw(VP, glfwGetTime()); 
+    // Scaling matrix to scale the cube down to a reasonable size.
+    mat4 cube_scale;
+    cube_scale << 0.25f, 0.0f,  0.0f,  0.0f,
+                  0.0f,  0.25f, 0.0f,  0.0f,
+                  0.0f,  0.0f,  0.25f, 0.0f,
+                  0.0f,  0.0f,  0.0f,  1.0f;
+    const float time = glfwGetTime();
+    mat4 cube_trafo = (Eigen::Affine3f(Eigen::AngleAxisf(2.0f * time, vec3::UnitY())) *
+                       Eigen::Affine3f(Eigen::Translation3f(vec3(0.75f, 0.0f, 0.0f))) *
+                       Eigen::Affine3f(Eigen::AngleAxisf(2.0f * time, vec3::UnitY()))).matrix();
+    //mat4 cube_trafo = Eigen::Affine3f(Eigen::AngleAxisf(time, vec3::UnitY())).matrix();
+    mat4 cube_model_matrix = cube_trafo * cube_scale;
+
+    cube.draw(trackball_matrix * cube_model_matrix, view_matrix, projection_matrix);
+
+    // Draw a quad on the ground.
+    mat4 quad_model_matrix = Eigen::Affine3f(Eigen::Translation3f(vec3(0.0f, -0.25f, 0.0f))).matrix();
+    grid.draw(trackball_matrix * quad_model_matrix, view_matrix, projection_matrix, time);
+
+    check_error_gl();
+
 }
 
-int main(int, char**) {
-    glfwInitWindowSize(window_width, window_height);
-    glfwCreateWindow("mirror_floor");
+// Transforms glfw screen coordinates into normalized OpenGL coordinates.
+vec2 transform_screen_coords(int x, int y) {
+    return vec2(2.0f * (float)x / WIDTH - 1.0f,
+                1.0f - 2.0f * (float)y / HEIGHT);
+}
+
+mat4 old_trackball_matrix;
+double y_o;
+
+void mouse_button(int button, int action) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        int x_i, y_i;
+        glfwGetMousePos(&x_i, &y_i);
+        vec2 p = transform_screen_coords(x_i, y_i);
+        trackball.begin_drag(p.x(), p.y());
+        old_trackball_matrix = trackball_matrix;  // Store the current state of the model matrix.
+    }else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+        int x_i, y_i;
+        glfwGetMousePos(&x_i, &y_i);
+        vec2 p = transform_screen_coords(x_i, y_i);
+        y_o = p.y();
+    }
+}
+
+void mouse_pos(int x, int y) {
+    if (glfwGetMouseButton(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        vec2 p = transform_screen_coords(x, y);
+        mat4 rotation_drag = trackball.drag(p.x(),p.y());
+        // TODO 3: Cacuclate 'trackball_matrix' given the return value of
+        // trackball.drag(...) and the value stored in 'old_trackball_matrix'.
+        // See also the mouse_button(...) function. 
+        trackball_matrix = old_trackball_matrix * rotation_drag;
+
+    
+
+    }
+
+    // Zoom
+    if (glfwGetMouseButton(GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+        // TODO 4: Implement zooming. When the right mouse button is pressed,
+        // moving the mouse cursor up and down (along the screen's y axis)
+        // should zoom out and it. For that you have to update the current
+        // 'view_matrix' with a translation along the z axis.
+
+        double x_p, y_p;
+        vec2 p = transform_screen_coords(x, y);
+        y_p = p.y();
+        Transform _M = Transform::Identity();
+        _M *= Eigen::Translation3f(0, 0, y_p - y_o);
+        y_o = y_p; 
+        mat4 trans_z_mat = _M.matrix();
+        view_matrix = trans_z_mat*view_matrix ;
+    }
+}
+
+int main(int, char**){
+    glfwInitWindowSize(WIDTH, HEIGHT);
+    glfwCreateWindow("Trackball");
     glfwDisplayFunc(display);
+    glfwSetWindowSizeCallback(&resize_callback);
+    glfwSetMouseButtonCallback(mouse_button);
+    glfwSetMousePosCallback(mouse_pos);
     init();
     glfwMainLoop();
-    return EXIT_SUCCESS;    
+    return EXIT_SUCCESS;
 }
---
+++ project/trackball.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "icg_common.h"
+#include <math.h>
+#include <Eigen/Dense>
+
+typedef Eigen::Transform<float,3,Eigen::Affine> Transform;
+
+class Trackball {
+public:
+    Trackball() : _radius(1.0f) {}
+
+    // This function is called when the user presses the left mouse button down.
+    // x, and y are in [-1, 1]. (-1, -1) is the bottom left corner while (1, 1)
+    // is the top right corner.
+    void begin_drag(float x, float y) {
+      _anchor_pos = vec3(x, y, 0.0f);
+      project_onto_surface(_anchor_pos);
+    }
+
+    // This fucntion is called while the user moves the curser around while the
+    // left mouse button is still pressed.
+    // x, and y are in [-1, 1]. (-1, -1) is the bottom left corner while (1, 1)
+    // is the top right corner.
+    // Returns the rotation of the trackball in matrix form.
+    mat4 drag(float x, float y) {
+      vec3 current_pos = vec3(x, y, 0.0f);
+      project_onto_surface(current_pos);
+
+
+      current_pos.normalize();
+      _anchor_pos.normalize();
+      vec3 N = _anchor_pos.cross(current_pos);
+      double teta = acos(_anchor_pos.dot(current_pos));
+      Transform _M = Transform::Identity();
+      _M *= Eigen::AngleAxisf(teta, N);
+
+      mat4 rotation = _M.matrix();
+      // TODO 3: Calculate the rotation given the projections of the anocher
+      // point and the current position. The rotation axis is given by the cross
+      // product of the two projected points, and the angle between them can be
+      // used as the magnitude of the rotation.
+      // You might want to scale the rotation magnitude by a scalar factor.
+      // p.s. No need for using complicated quaternions as suggested inthe wiki
+      // article.
+      return rotation;
+    }
+
+private:
+    // Projects the point p (whose z coordiante is still empty/zero) onto the
+    // trackball surface. If the position at the mouse cursor is outside the
+    // trackball, use a hyberbolic sheet as explained in:
+    // https://www.opengl.org/wiki/Object_Mouse_Trackball.
+    // The trackball radius is given by '_radius'.
+    void project_onto_surface(vec3& p) const {
+
+      // TODO 2: Implement this function. Read above link for details.
+      double flag = pow(p.x(),2) + pow(p.y(),2);
+
+      if (flag <= pow(_radius,2)/2.0) {
+        p.z() = sqrt(pow(_radius,2) - flag);
+      } else {
+        p.z() = (pow(_radius,2)/2.0)/sqrt(flag);
+      }
+      
+    }
+
+    double radians(double degree) {
+      return degree * M_PI/180;
+    }
+
+    float _radius;
+    vec3 _anchor_pos;
+    mat4 _rotation;
+};
