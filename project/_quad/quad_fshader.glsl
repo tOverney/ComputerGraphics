b8aa837533c02506fef535a702d918224f9a2cbf
tOverney <tristan.overney@epfl.ch>
2015-04-28 15:34:53 +0200

    correct terrain generation with overlapping of 3 fbm

--- project/_quad/quad_fshader.glsl
+++ project/_quad/quad_fshader.glsl
@@ -1,67 +1,92 @@
 #version 330 core
+
+const int GRID_SIZE = 512;
+const int NB_CELLS = GRID_SIZE * GRID_SIZE;
+
 out vec3 color;
 in vec2 uv;
 uniform sampler2D tex;
-//uniform float colors[][];
 
-const int GRID_SIZE = 2;
-//const float[] VECTORS = float[](0, 0.2, 0.4, 0.6);
-const int[] permutation = int[](151,160,137,91,90,15,
-   131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-   190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-   88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-   77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-   102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-   135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-   5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-   223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-   129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-   251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-   49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-   138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-   );
 
-float interpolationFunction(float value){
-    return pow(cos(value),2);
+float interpolationFunction(float t){
+    return t * t * t * (10.0f - t * ( 15.0f - 6.0f * t)); //6.0f * pow(value, 5) - 15.0f * pow(value, 4) + 10 * pow;
 }
-float mix(float x, float y, float alpha){
-    return (1-alpha)*x+alpha*y;
 
+// float rand(vec2 point) {
+//     return fract(sin(dot(point ,vec2(12.9898,78.233))) * 43758.5453);
+// }
+
+float rand(vec2 n) { 
+    return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
 }
+
+
+float noise(vec2 p){
+    vec2 ip = floor(p);
+    vec2 u = fract(p);
+    u = u*u*(3.0-2.0*u);
+
+    float res = mix(
+        mix(rand(ip),rand(ip+vec2(1.0,0.0)),u.x),
+        mix(rand(ip+vec2(0.0,1.0)),rand(ip+vec2(1.0,1.0)),u.x),u.y);
+    return res*res;
+}
+
+float fbm(vec2 x) {
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.50));
+    for (int i = 0; i < 5; ++i) {
+        v += a * noise(x);
+        x = rot * x * 2.0 + shift;
+        a *= 0.5;
+    }
+    return v;
+}
+
 
 void main() {
-    vec2 cell = uv * GRID_SIZE;
-   // float color_val = colors[int(cell.x)][int(cell.y)];
+    // vec2 pos = 64*uv;
+    // // float color_val = colors[int(cell.x)][int(cell.y)];
+    // vec2 p00 = vec2(floor(pos.x), floor(pos.y));
+    // vec2 p01 = vec2(p00.x, p00.y + 1);
+    // vec2 p10 = vec2(p00.x + 1, p00.y);
+    // vec2 p11 = vec2(p10.x, p01.y);
 
-    //Find celle that contains the point
-    int cellRow = int(cell.y);
-    int cellColumn = int(cell.x);
+    // //Find celle that contains the point
+    // vec2 g00 = grand(p00);
+    // vec2 g01 = grand(p01);
+    // vec2 g10 = grand(p10);
+    // vec2 g11 = grand(p11);
+    
 
-    //calculate random gradients
-    vec2 g00=vec2(permutation[cellRow], permutation[cellColumn])/255;
-    vec2 g01=vec2(permutation[cellRow+1], permutation[cellColumn])/255;
-    vec2 g10=vec2(permutation[cellRow], permutation[cellColumn+1])/255;
-    vec2 g11=vec2(permutation[cellRow+1], permutation[cellColumn+1])/255;
-
-
-    // calculate random gradient at cell corners
-    vec2 op = vec2(cell.x-int(cell.x), cell.y-int(cell.y));
-    vec2 ap = op;
-    vec2 bp = op - vec2(1,0);
-    vec2 cp = op - vec2(0,1);
-    vec2 dp = op - vec2(1,1);
-
-    float s = dot(g00, ap);
-    float t = dot(g10,bp);
-    float u = dot(g01, cp);
-    float v = dot(g11, dp);
+    // //calculate random gradients
+    // // vec2 g00=vec2(permutation[cellRow], permutation[cellColumn])/255;
+    // // vec2 g01=vec2(permutation[cellRow+1], permutation[cellColumn])/255;
+    // // vec2 g10=vec2(permutation[cellRow], permutation[cellColumn+1])/255;
+    // // vec2 g11=vec2(permutation[cellRow+1], permutation[cellColumn+1])/255;
 
 
+    // // calculate random gradient at cell corners
+    // vec2 ap = pos - p00;
+    // vec2 bp = pos - p01;
+    // vec2 cp = pos - p10;
+    // vec2 dp = pos - p11;
 
-    float st = mix(s,t,interpolationFunction(cell.x));
-    float uv2 = mix(u, v, interpolationFunction(cell.x));
-    float noise = mix(st, uv2, interpolationFunction(cell.y));
+    // float s = dot(g00, ap)* 0.5 + 0.5;
+    // float t = dot(g10, bp)* 0.5 + 0.5;
+    // float u = dot(g01, cp) * 0.5 + 0.5;
+    // float v = dot(g11, dp) * 0.5 + 0.5;
 
+
+
+    // float st = mix(s,t,interpolationFunction(ap.x));
+    // float uv2 = mix(u, v, interpolationFunction(ap.x));
+    // float noise = mix(st, uv2, interpolationFunction(ap.y));
+
+    float noise = 1.5 *(fbm(5 *uv) + fbm(10 * uv) + fbm(2 * uv))/3;
 
     color = vec3(noise,noise,noise);
 }
--- project/_terrain/terrain_vshader.glsl
+++ project/_terrain/terrain_vshader.glsl
@@ -1,17 +1,21 @@
 #version 330 core
 
 uniform mat4 mvp;
+uniform sampler2D height_map;
 
 in vec2 position;
 out vec2 uv;
+out float height_v;
 
 
 void main() {
     uv = (position + vec2(1.0, 1.0)) * 0.5;
 
 
+    float height = texture(height_map, uv).r;
+    height_v = height;
 
-    vec3 pos_3d = vec3(position.x, 0, -position.y);
+    vec3 pos_3d = vec3(position.x, height, -position.y);
 
     gl_Position = mvp* vec4(pos_3d, 1.0);
 }
